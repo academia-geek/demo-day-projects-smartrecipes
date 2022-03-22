e5b1475f49c64df9358ac58892498c59d60d85f0
David-bv <david.bolivarv4@gmail.com>
2022-03-22 17:52:39 -0500

    Merge Charif

--- src/index.js
+++ src/index.js
@@ -14,10 +14,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-<<<<<<< HEAD
-=======
       <GoblalStyles />
->>>>>>> 7a348b2f19302e48e1f415266228519ccfd47656
       <AppRouter />
     </React.StrictMode>
   </Provider>,
