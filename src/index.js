01a6106439960e338759a2071eedb86df441f10c
David Charif <davidcharif@hotmail.com>
2022-03-22 17:49:34 -0500

    stuled before

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import { store } from './redux/store/store';
-
-
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GoblalStyles } from './styles/styledComponents/goblalStyles';
 
