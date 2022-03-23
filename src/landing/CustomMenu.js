940806dd5568c1442b9042eeeb9a4206d8f4efb1
David-bv <david.bolivarv4@gmail.com>
2022-03-23 16:06:11 -0500

    Update devel json server

--- src/landing/CustomMenu.js
+++ src/landing/CustomMenu.js
@@ -11,6 +11,7 @@ const CustomMenu = () => {
             </Container>
             <Container>
                 cards
+                
             </Container>
             <Container style={{ backgroundColor: 'blue' }}>
                 <Link to='/login' style={{ textDecoration: 'none' }}>
