264e307a41567b957248263815ecd2633df7c1d2
David-bv <david.bolivarv4@gmail.com>
2022-03-24 12:28:30 -0500

    Heroku changes

--- src/components/afterLogin/pagPrincipalLog/calendarioPagPrincipal/Calendario.jsx
+++ src/components/afterLogin/pagPrincipalLog/calendarioPagPrincipal/Calendario.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { CalendarioStyled } from '../../../../styles/styledComponents/PagPrincipalStyles'
 // import Calendar from 'react-calendar'
 
--- src/components/landing/CardMenu.js
+++ src/components/landing/CardMenu.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Col, Container, Row } from 'react-bootstrap'
+import { Card, Col, Row } from 'react-bootstrap'
 
 const CardMenu = ({ data }) => {
 
