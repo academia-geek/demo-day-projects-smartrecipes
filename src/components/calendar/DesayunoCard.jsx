f6f6312047e1d49ed022c9a8c2e3cd0644360622
David Charif <davidcharif@hotmail.com>
2022-03-31 18:08:25 -0500

    detalles

--- src/components/calendar/DesayunoCard.jsx
+++ src/components/calendar/DesayunoCard.jsx
@@ -1,48 +1,30 @@
-import axios from 'axios';
-import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { Card } from 'react-bootstrap';
-import { urlRD } from '../../utilities/urlRecipes';
 
-const DesayunoCard = () => {
+import React from "react";
 
-    const [dishes, setDishes] = useState([]);
+import { Card } from "react-bootstrap";
 
-    useEffect(() => {
-        async function fetchData() {
-            const dataServer = await axios.get(urlRD);
-                console.log(dataServer.data)
-            setDishes(dataServer.data)
-        }
-        fetchData()
-    }, [])
 
+const DesayunoCard = ({ dish }) => {
+  console.log(dish)
   return (
-    <div>
-
-      {dishes.map((dish, i) => (
-        <div key={`dish-${i}`} >
-                <Card className="container-card">
-                    {/* <Card.Img variant="top" src={dish.image} className="imagen-menu-card" /> */}
-                    <Card.Body className="body-menu-card">
-                        <Card.Title className="title-menu">{dish.title}</Card.Title>
-                        <Card.Text>
-                            Preparation Time: {dish.readyInMinutes}' 
-                            <br/>
-                           
-                            Diet type: {dish.diets.join(', ')}
-                            <br/>
-                        </Card.Text>
-                      
-                    </Card.Body>
-                </Card>      
-
-        </div>
-      ))}
-
-    </div>
+    
+     <div key={`dish`}> <Card className="container-card">
+        {/* <Card.Img variant="top" src={dish.image} className="imagen-menu-card" /> */}
+        <Card.Body className="body-menu-card">
+          <Card.Title className="title-menu">{dish.title}</Card.Title>
+          <Card.Text>
+            Preparation Time: {dish.readyInMinutes}'
+            <br />
+            Diet type: {dish.diets.join(", ")}
+            <br />
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>   
+     
   )
-}
+    }
+  
 
-export default DesayunoCard
+
+export default DesayunoCard;
