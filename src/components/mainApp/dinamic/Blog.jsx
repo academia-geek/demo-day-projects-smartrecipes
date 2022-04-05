d0af96fca4d736ef80f9f770ef0c7b5f0c15918d
Sara <saritapz01@hotmail.com>
2022-04-05 00:10:31 -0500

    actualizaciones en blog

--- src/components/mainApp/dinamic/Blog.jsx
+++ src/components/mainApp/dinamic/Blog.jsx
@@ -1,31 +1,30 @@
 import React from 'react'
-import { BlogContainer } from '../../../styles/styledComponents/BlogContainer'
+import { AnchorBlog, ArticleBlog, BlogContainer, ImagenBlog, MainBlog, TitleBlogInicial } from '../../../styles/styledComponents/BlogStyle'
 
 const Blog = () => {
   return (
     <BlogContainer>
-
-    <main className="contenedor seccion contenido-centrado">
         <h1>Blog: Zona Salud</h1>
 
-        <article className="entrada-blog">
-            <div>
-                <img alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649038093/DemoDay/maxresdefault_oyjgxn.jpg' />
-            </div>
-            <div>
-                <a href="entrada.html" class="texto-blog">
-                    <h4 class="texto-blog">Cocina zero-waste: Aprende 3 recetas deliciosas con cáscara de plátano</h4>
-                </a>
-                    <p class="texto-blog">Escrito el: <span>20/03/2022</span> por: <span>Admin</span> </p>
-                    <p class="texto-blog">
-                      ¿Te animas a intentar tener una cocina zero-waste? De esta forma podrás comer sano y ayudar al medio ambiente. Por ello, te dejamos hoy estas 3 increíbles recetas preparadas con cáscaras de plátanos.
-                    </p>
-            </div>
-        </article>
+    <MainBlog>
+        <ArticleBlog>
 
-        <article class="entrada-blog">
+                <ImagenBlog alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649038093/DemoDay/maxresdefault_oyjgxn.jpg' />
+
+            <>
+                <AnchorBlog href="entrada.html" class="texto-blog">
+                    <h4>Cocina zero-waste: Aprende 3 recetas deliciosas con cáscara de plátano</h4>
+                </AnchorBlog>
+                    <p>Escrito el: <span>20/03/2022</span> por: <span>Admin</span> </p>
+                    <TitleBlogInicial>
+                      ¿Te animas a intentar tener una cocina zero-waste? De esta forma podrás comer sano y ayudar al medio ambiente. Por ello, te dejamos hoy estas 3 increíbles recetas preparadas con cáscaras de plátanos.
+                    </TitleBlogInicial>
+            </>
+        </ArticleBlog>
+
+        <ArticleBlog>
             <div>
-                <img alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649039514/DemoDay/_109785495_0a.gettyimages-1141690899_mlaxxf.jpg' />
+                <ImagenBlog alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649039514/DemoDay/_109785495_0a.gettyimages-1141690899_mlaxxf.jpg' />
             </div> 
             <div>
                 <a href="entrada.html" class="texto-blog">
@@ -36,11 +35,11 @@ const Blog = () => {
                       Si últimamente sientes mucho cansancio en el día o piensas que no estás durmiendo correctamente, presta atención a estos consejos nutricionales para descansar mejor por las noches.
                     </p>
             </div>
-        </article>
+        </ArticleBlog>
 
-        <article class="entrada-blog">
+        <ArticleBlog>
             <div>
-                <img alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649039347/DemoDay/alimentos-frigorifico_ccjatt.jpg'/>
+                <ImagenBlog alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649039347/DemoDay/alimentos-frigorifico_ccjatt.jpg'/>
             </div>
             <div>
                 <a href="entrada.html" class="texto-blog">
@@ -51,11 +50,11 @@ const Blog = () => {
                       Alimentos que no necesitan refrigeración. Porque es muy importante que reservemos ese espacio para lo que realmente lo requiera.
                     </p>
             </div>
-        </article>
+        </ArticleBlog>
 
-        <article class="entrada-blog">
+        <ArticleBlog>
             <div>
-                <img alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649039244/DemoDay/recetas-divertidas-para-ni%C3%B1os-1_qzque0.jpg' />
+                <ImagenBlog alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649039244/DemoDay/recetas-divertidas-para-ni%C3%B1os-1_qzque0.jpg' />
             </div> 
             <div>
                 <a href="entrada.html" class="texto-blog">
@@ -66,9 +65,9 @@ const Blog = () => {
                         A continuación, te sugerimos algunas herramientas fáciles de aplicar en el día a día, que nos ayudarán a alcanzar nuestros objetivos nutricionales.
                     </p>
             </div>
-        </article>
+        </ArticleBlog>
 
-    </main>
+    </MainBlog>
 
     </BlogContainer>
   )
