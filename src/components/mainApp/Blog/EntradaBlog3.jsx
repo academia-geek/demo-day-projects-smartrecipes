def424dec15b588debeca2d0f9b4c3399cc70e4e
Sara <saritapz01@hotmail.com>
2022-04-05 15:16:21 -0500

    Adición de entradas de blog

---
+++ src/components/mainApp/Blog/EntradaBlog3.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { ImagenBlog, MainBlog, TitleBlogInicial } from '../../../styles/styledComponents/BlogStyle'
+
+const EntradaBlog3 = () => {
+  return (
+    <>
+        <MainBlog>
+        
+        <h1>Consejos nutricionales para dormir y descansar mejor</h1>
+       
+        <ImagenBlog alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649039514/DemoDay/_109785495_0a.gettyimages-1141690899_mlaxxf.jpg' />
+
+        <p>Escrito el: <span>02/04/2022</span> por: <span>Admin</span> </p>
+
+        <TitleBlogInicial>
+          Si últimamente sientes mucho cansancio en el día o piensas que no estás durmiendo correctamente, presta atención a estos consejos nutricionales para descansar mejor por las noches.
+        </TitleBlogInicial>
+
+        <h3>¿Por qué es importante dormir?</h3>
+        <p>
+        Para nuestro organismo es vital descansar adecuadamente, ya que durante este periodo es cuando ocurre la reparación y recuperación celular, la consolidación de la memoria de largo plazo, y el balance hormonal encargado de nuestra salud general.
+        </p>
+
+        <h4>Higiene del sueño</h4>
+        <p>
+        Una forma de evaluar nuestro dormir es a través de la higiene del sueño. Este es un concepto que se refiere a todas las acciones que realizamos mientras estamos despiertos que pueden afectar nuestro buen dormir. Mientras identifiquemos menos acciones en nuestro diario vivir mejor es nuestra higiene del sueño. Algunas de las actividades que se sugieren evitar son:
+        </p>
+
+        <ul>
+          <li>Fumar cigarrillos en horas próximas a dormir</li>
+          <li>Trabajar, estudiar, o comer en la cama.</li>
+          <li>Dormir siestas muy prolongadas (más de 20-30 minutos) durante el día.</li>
+          <li>No tener un horario fijo para ir a dormir o desvelarse muy seguido.</li>
+          <li>Ir a dormir y mirar pantallas como la del teléfono, televisión o computador.</li>
+        </ul>
+
+        <p>
+        Desde el punto de vista nutricional y la calidad del sueño, existe evidencia sólida de que llevar una mala higiene del sueño se asocia a un aumento en las probabilidades de desarrollar obesidad, enfermedades crónicas como diabetes, hipertensión arterial, y obesidad.
+        </p>
+
+        <p>
+        Igualmente, se conoce que la obesidad por sí sola conlleva alteraciones del sueño, por lo que existe una relación bidireccional de causa-efecto entre el sueño y la nutrición.
+        </p>
+
+        <p>Recomendaciones relacionadas a lo nutricional</p>
+
+        <ul>
+          <li>Mantener un peso saludable a través de una dieta balanceada y realización de actividad física diariamente.</li>
+          <li>No consumir bebidas con cafeína (café, té y bebidas energéticas) en exceso, o luego de las 6 de la tarde.</li>
+          <li>Tener un periodo de 3-4 horas de diferencia entre la última comida e ir a dormir.</li>
+          <li>Se ha identificado que la vitamina D tiene una asociación positiva con la calidad del sueño. Si bien muy pocos alimentos aportan esta vitamina naturalmente, se recomienda el consumo de pescados, yema de huevo, lácteos, y procurar diariamente la exposición solar durante 15 minutos en horarios de menor radiación.</li>
+          <li>Consumir alimentos ricos en triptófano, como lácteos, piña, plátano, frutos secos, porotos, pescados, pavo y pollo, ya que este aminoácido es un precursor de la melatonina, conocida como la hormona responsable del ciclo sueño-vigilia.</li>
+        </ul>
+
+        </MainBlog>
+    </>
+  )
+}
+
+export default EntradaBlog3
---
+++ src/components/mainApp/Blog/EntradaBlog4.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import { ImagenBlog, MainBlog, TitleBlogInicial } from '../../../styles/styledComponents/BlogStyle'
+
+const EntradaBlog3 = () => {
+  return (
+    <>
+        <MainBlog>
+        <h1>Alimentos que no necesitan refrigeración</h1>     
+        <ImagenBlog alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649039347/DemoDay/alimentos-frigorifico_ccjatt.jpg'/>
+
+        <p>Escrito el: <span>30/03/2022</span> por: <span>Admin</span> </p>
+        <TitleBlogInicial>
+          Alimentos que no necesitan refrigeración. Porque es muy importante que reservemos ese espacio para lo que realmente lo requiera.
+        </TitleBlogInicial>
+
+        <ul>
+          <li>
+          Huevos
+Solo los huevos que han sido lavados con agua deben conservarse en la nevera. Si no han sido lavados (solo limpiados), pueden conservarse sin problemas a temperatura ambiente, como vemos en los supermercados.
+          </li>
+
+          <li>
+Plátanos
+Los plátanos necesita luz y una temperatura alta para madurar. Si los metes en la nevera no solo no madurarán, sino que se pudrirán antes.
+          </li>
+
+          <li>
+          Sandías
+Los expertos lo desanconsejan si está sin abrir, porque pierde betacarotenos. Una vez abierta se puede conservar en el frigorífico, pero hay que cubrirla con papel de aluminio. Si duda, uno de los alimentos que no debes meter en la nevera más sorprendentes.
+          </li>
+
+          <li>
+          Chocolate
+Mucha gente guarda el chocolate en la nevera, especialmente en verano, para que no se derrita. No es malo en sí mismo, pero el frío altera por completo el sabor del chocolate. Mételo al refrigerador solo si se está derritiendo por completo.
+          </li>
+
+          <li>
+          Frutas tropicales
+kiwi, mango, damasco y otras frutas que provienen de zonas cálidas, están acostumbradas a las altas temperaturas. Además necesitan luz y calor para madurar. Si los metes en la nevera pierden nutrientes.
+          </li>
+
+          <li>
+          Conservas
+Su propio nombre ya lo indica. Productos como el atún enlatado, los pepinillos en vinagre o la mermelada son productos pensados para perdurar en el tiempo. No necesitan frigorífico, aunque como casi todos los alimentos que no necesitan madurar, hay que conservarlos en lugares secos, frescos y oscuros.
+          </li>
+
+          <li>
+          Cítricos
+La naranja, el limón, y la mandarina se pudren con la humedad del frigorífico. Es uno de los principales alimentos que no necesitan nevera.
+          </li>
+
+          <li>
+          Papas
+Si metes las papas en la nevera convertirán el almidón en azúcar más rápidamente, y se estropearán antes. Deben conservarse en un lugar seco y oscuro, sin lavarlas.
+          </li>
+
+          <li>
+          Aceite, miel
+Estos líquidos se endurecen, y cristalizan con las bajas temperaturas, aunque también se conservan más.
+          </li>
+
+          <li>
+          Ketchup o mostazas
+Los ácidos de estas salsas son altamente conservantes. Pueden ser usadas muchos días después de abrir el bote, sin necesidad de frigorífico.
+          </li>
+
+          <li>
+          Café
+Es un grano seco y tostado, no necesita frío para su conservación.
+          </li>
+
+          <li>Tomates
+Aunque es cierto que los tomates aguantan más en la nevera y no se estropean, las bajas temperaturas impiden que maduren. También pierden sabor y textura.
+</li>
+
+          <li>Pepinos
+Los pepinos aguantan bien las altas temperaturas, gracias a su gruesa piel. Si los metes en la nevera se ablandarán y se pudrirán antes.
+</li>
+
+          <li>Pimientos
+Pierden el color y la textura a bajas temperaturas.
+</li>
+
+          <li>Especias
+La mayoría de la especias son granos y hojas que han sufrido un proceso de secado. Ya no tienen agua así que se conservan a la perfección a temperatura ambiente y a oscuras. Al contrario, el frío y la humedad del refrigerador hace que pierdan sus propiedades.
+</li>
+
+        </ul>
+        
+        </MainBlog>
+    </>
+  )
+}
+
+export default EntradaBlog3
