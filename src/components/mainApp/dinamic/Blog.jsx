bf6147c615b442e2f4dcd5db3737dc8012fbb3d8
Sara <saritapz01@hotmail.com>
2022-04-05 10:03:56 -0500

    actualizaciones en blog

--- src/components/mainApp/dinamic/Blog.jsx
+++ src/components/mainApp/dinamic/Blog.jsx
@@ -23,48 +23,44 @@ const Blog = () => {
         </ArticleBlog>
 
         <ArticleBlog>
-            <div>
                 <ImagenBlog alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649039514/DemoDay/_109785495_0a.gettyimages-1141690899_mlaxxf.jpg' />
-            </div> 
-            <div>
-                <a href="entrada.html" class="texto-blog">
+             
+            <>
+                <AnchorBlog href="entrada.html" class="texto-blog">
                     <h4 class="texto-blog">Consejos nutricionales para dormir y descansar mejor</h4>
-                </a>
-                    <p class="texto-blog">Escrito el: <span>02/04/2022</span> por: <span>Admin</span> </p>
-                    <p class="texto-blog">
+                </AnchorBlog>
+                    <p>Escrito el: <span>02/04/2022</span> por: <span>Admin</span> </p>
+                    <TitleBlogInicial>
                       Si últimamente sientes mucho cansancio en el día o piensas que no estás durmiendo correctamente, presta atención a estos consejos nutricionales para descansar mejor por las noches.
-                    </p>
-            </div>
+                    </TitleBlogInicial>
+            </>
         </ArticleBlog>
 
         <ArticleBlog>
-            <div>
+            
                 <ImagenBlog alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649039347/DemoDay/alimentos-frigorifico_ccjatt.jpg'/>
-            </div>
-            <div>
-                <a href="entrada.html" class="texto-blog">
+            <>
+                <AnchorBlog href="entrada.html" class="texto-blog">
                     <h4 class="texto-blog">Alimentos que no necesitan refrigeración</h4>
-                </a>
-                    <p class="texto-blog">Escrito el: <span>30/03/2022</span> por: <span>Admin</span> </p>
-                    <p class="texto-blog">
+                </AnchorBlog>
+                    <p>Escrito el: <span>30/03/2022</span> por: <span>Admin</span> </p>
+                    <TitleBlogInicial>
                       Alimentos que no necesitan refrigeración. Porque es muy importante que reservemos ese espacio para lo que realmente lo requiera.
-                    </p>
-            </div>
+                    </TitleBlogInicial>
+            </>
         </ArticleBlog>
 
         <ArticleBlog>
-            <div>
                 <ImagenBlog alt='' src='https://res.cloudinary.com/sarapalacio01/image/upload/v1649039244/DemoDay/recetas-divertidas-para-ni%C3%B1os-1_qzque0.jpg' />
-            </div> 
-            <div>
-                <a href="entrada.html" class="texto-blog">
+            <>
+                <AnchorBlog href="entrada.html" class="texto-blog">
                     <h4 class="texto-blog">4 tips saludables para organizar tus menús semanales</h4>
-                </a>
-                    <p class="texto-blog">Escrito el: <span>20/12/2021</span> por: <span>Admin</span> </p>
-                    <p class="texto-blog">
+                </AnchorBlog>
+                    <p>Escrito el: <span>20/12/2021</span> por: <span>Admin</span> </p>
+                    <TitleBlogInicial>
                         A continuación, te sugerimos algunas herramientas fáciles de aplicar en el día a día, que nos ayudarán a alcanzar nuestros objetivos nutricionales.
-                    </p>
-            </div>
+                    </TitleBlogInicial>
+            </>
         </ArticleBlog>
 
     </MainBlog>
